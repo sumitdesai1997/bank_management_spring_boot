c9645857f8c595b779c3bef9f38840c8e4e263e5
sumit <sumit@DESKTOP-NU5FKLT>
2022-05-13 11:56:43 -0400

    initial commit

---
+++ src/main/java/com/bankmanagement/BankManagementSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bankmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankmanagement/BankManagementSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
